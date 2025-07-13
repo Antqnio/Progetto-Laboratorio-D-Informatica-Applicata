78fb8a04f0dbfbd3eb80ad7fc18908bb503749c8
Antonio Querci <a.querci6@studenti.unipi.it>
2025-07-13 18:24:33 +0200

    Refactor flask_client.py

--- client/flask_client.py
+++ client/flask_client.py
@@ -93,11 +93,14 @@ def index():
         - For POST requests: Returns a JSON response indicating the result of the action.
     """
 
+    
+    # List of available configuration files (without .json extension)
+    config_files = [f[:-5] for f in os.listdir(CONFIG_DIR) if f.endswith(".json")]
+    
+    
     if request.method == "POST":
         global gesture_to_command
 
-        # List of available configuration files (without .json extension)
-        config_files = [f[:-5] for f in os.listdir(CONFIG_DIR) if f.endswith(".json")]
         
         # Used only for the "save" action
         # request.form.get("config_name_select") is used when the user selects a config from the dropdown
@@ -134,7 +137,9 @@ def index():
         else:
             print(f"[ERROR] Unknown action: {action}")
             return jsonify({"status": "error", "message": "Unknown action."}, 400)
+        
     # GET: only when the user opens the page for the first time
+    global recognition_active
     return render_template(
         "index.html",
         gestures=GESTURES,
